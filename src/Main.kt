b47637b13c63ee0613f690ccba30a4499477ad81
VelazqueZzz <m.italy@vk.com>
2026-09-25 15:07:37 +0300

    Initial commit

---
+++ src/Main.kt
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    val move = "D2-D4;0"
+    val parts = move.split(";")
+    val cells = parts[0].split("-")
+    val from = cells[0]
+    val to = cells[1]
+    val moveNumber = parts[1]
+    println("Откуда: $from")
+    println("Куда: $to")
+    println("Номер хода: $moveNumber")
+}
